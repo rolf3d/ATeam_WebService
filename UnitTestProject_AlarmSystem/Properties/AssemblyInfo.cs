0a06423aa8b9782750c5750403552c74492322b4
Casper Sørensen <casp677d@edu.easj.dk>
2017-11-22 11:19:21 +0100

    Tilføjede et unittest projekt og lavede en standart test.

---
+++ UnitTestProject_AlarmSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_AlarmSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_AlarmSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95ee5445-91c3-4bd5-b440-12ab21ec1d73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject_AlarmSystem/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebService_AlarmSystem;
+
+namespace UnitTestProject_AlarmSystem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Service1 ggg = new Service1();
+
+            ggg.GetData(6);
+            string gggg = "You entered: 6";
+
+            Assert.AreEqual(ggg.GetData(6), gggg);
+        }
+    }
+}
